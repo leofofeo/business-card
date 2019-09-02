06a74a131c95b6457bd9319ed2292d8b9157bb76
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-09-01 23:44:23 -0500

    add fonts, render image, name, and title

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());    
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.deepPurple[700],
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                renderAvatar(),
+                renderName(),
+                renderTitle(),
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+}
+
+Widget renderAvatar() {
+  return CircleAvatar(
+    radius: 50.0,
+    backgroundImage: AssetImage('images/me.jpg'),
+  );
+}
+
+Widget renderName() {
+  return Text(
+    'Leo Rubiano',
+    style: TextStyle(
+      color: Colors.white,
+      fontSize: 40.0,
+      fontFamily: 'Quicksand',
+      fontWeight: FontWeight.w700,
+    ),
+  );
+}
+
+Widget renderTitle() {
+  return Text(
+    'DEVELOPER',
+    style: TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+      fontFamily: 'SourceSansPro',
     ),
   );
 }
