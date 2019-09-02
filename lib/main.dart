1cf58d9d4e3e76c1cd9d69462737832faf48af53
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-09-02 00:14:56 -0500

    add in telephone and email, add additionaly styling

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
                 renderAvatar(),
                 renderName(),
                 renderTitle(),
+                renderEmail(),
+                renderPhoneNumber(),
               ],
             ),
           ),
@@ -53,7 +55,70 @@ Widget renderTitle() {
     style: TextStyle(
       color: Colors.white,
       fontSize: 20.0,
+      letterSpacing: 2.5,
       fontFamily: 'SourceSansPro',
+    ),
+  );
+}
+
+Widget renderEmail() {
+  return Container(
+    width: 300.0,
+    padding: EdgeInsets.all(15.0),
+    margin: EdgeInsets.only(top: 20.0),
+    color: Colors.deepPurple[300],
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(right: 15.0),
+          child: Icon(
+            Icons.email,
+            size: 25.0,
+            color: Colors.amber[300], 
+          ),
+        ),
+        Text(
+          'hey@leorubiano.com',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Quicksand',
+            fontSize: 22.0,
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget renderPhoneNumber() {
+  return Container(
+    width: 300.0,
+    padding: EdgeInsets.all(15.0),
+    margin: EdgeInsets.only(top: 20.0),
+    color: Colors.deepPurple[300],
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(right: 15.0),
+          child: Icon(
+            Icons.phone,
+            size: 25.0,
+            color: Colors.amber[300], 
+          ),
+        ),
+        Text(
+          '781-428-0644',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Quicksand',
+            fontSize: 22.0,
+          ),
+        )
+      ],
     ),
   );
 }
